6767a928cdf5d78e8d6575ed498241921c4e70bf
pauromaoluche <pauluci125romao.pedro@hotmail.com>
2025-06-03 19:11:40 -0300

    removacao da virgula

--- services/gasto_basal.py
+++ services/gasto_basal.py
@@ -6,7 +6,7 @@ def calculo_basal(input: CalculoBasal) -> float:
         case SexoEnum.homem:
             return 66.5 + (13.75 * input.peso) + (5 * input.altura) - (6.75 * input.idade)
         case SexoEnum.mulher:
-            return 665 + (9.6 * input.peso) + (1,8 * input.altura) - (4.7 * input.idade)
+            return 665 + (9.6 * input.peso) + (1.8 * input.altura) - (4.7 * input.idade)
         
 def gasto_energetico_total(input: GastoTotal) -> float:
     basal = calculo_basal(input)
