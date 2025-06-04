4bb35ea7f597f664476ba2af073d6f37a95a8f0b
pauromaoluche <pauluci125romao.pedro@hotmail.com>
2025-06-04 10:16:40 -0300

    Utilizando novo metodo de peso referencia, e ajustes

--- services/proteina_service.py
+++ services/proteina_service.py
@@ -1,32 +1,21 @@
 from schemas.base import CalculoDieta
-from services.corporal import estimar_perc_gordura, calc_massa_magra
+from services.corporal import estimar_perc_gordura, calc_massa_magra, get_peso_referencia
 from schemas.enum import NivelAtividadeEnum, ObjetivoEnum, EstadoFisicoEnum
 
 def calcular_proteina(input: CalculoDieta): 
+    peso = get_peso_referencia(input)
     proteina_por_kg = get_proteina_por_kg(input.atividade)
-    
-    if hasattr(input, 'percentual_gordura') and input.percentual_gordura >= 25:
-        # Faz a estimativa de quanto a pessoa tem de massa magra, para fazer o calculo de proteina, caso a pessoa tenha mais de 30% de gordura
-        massa_magra = calc_massa_magra(input.percentual_gordura, input.peso)
-        input.peso = massa_magra["massa_magra"]
-    elif hasattr(input, 'estado_fisico') and input.estado_fisico >= EstadoFisicoEnum.sobrepeso:
-        estimar_gordura = estimar_perc_gordura(input.estado_fisico, input.sexo)
-        
-        massa_magra = calc_massa_magra(estimar_gordura, input.peso)
-        input.peso = massa_magra["massa_magra"]
-        
+      
     if isinstance(proteina_por_kg, tuple):
-        match input.objetivo:
-            case ObjetivoEnum.emagrecer:
-                valor_proteina = proteina_por_kg[0]
-            case ObjetivoEnum.manter:
-                valor_proteina = sum(proteina_por_kg) / 2
-            case ObjetivoEnum.hipertrofia:
-                valor_proteina = proteina_por_kg[1]
+        valor_proteina = (
+            proteina_por_kg[0] if input.objetivo == ObjetivoEnum.emagrecer else
+            proteina_por_kg[1] if input.objetivo == ObjetivoEnum.hipertrofia else
+            sum(proteina_por_kg) / 2
+        )
     else:
         valor_proteina = proteina_por_kg[0]
     
-    return valor_proteina * input.peso
+    return round(valor_proteina * peso, 2)
 
 
 def get_proteina_por_kg(atividade: NivelAtividadeEnum) -> tuple[float, float]:
