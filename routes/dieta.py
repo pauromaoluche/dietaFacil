37223367d71f9f3ade7db6010026eb96061abf43
pauromaoluche <pauluci125romao.pedro@hotmail.com>
2025-06-09 16:12:54 -0300

    Ajustes na documentacao

--- routes/dieta.py
+++ routes/dieta.py
@@ -16,13 +16,36 @@ router = APIRouter(
 Este endpoint calcula os macronutrientes necessários para uma dieta personalizada com base nas informações fornecidas pelo usuário. 
 
 ### Parâmetros de entrada:
+- **atividade**: Tipo de atividade:
+  - sedentario (nada ou pouca atividade fisica) = 0
+  - atleta_esporadico (faz atividade fisica, mas de maneira não aleatorio) = 1
+  - resistencia (corrida, crossfit, etc...) = 2
+  - misto (faz de tudo) = 3
+  - forca (musculacao ou atividades que precisam de força) = 4
+- **objetivo**: Objetivo do usuário: 
+  - emagrecer = 0
+  - manter = 1
+  - hipertrofia = 2
 - **peso**: Peso do usuário (kg)
+- **sexo**: Sexo do usuário
 - **altura**: Altura do usuário (cm)
 - **idade**: Idade do usuário
-- **sexo**: Sexo do usuário (masculino ou feminino)
-- **frequencia_atividade**: Nível de atividade física do usuário (sedentário, moderado, ativo)
-- **objetivo**: Objetivo do usuário (emagrecer, hipertrofia ou manter peso)
 - **nivel_calorico**: Percentual de ajuste calórico para atingir o objetivo (ex: de 0 a 50%)
+- **frequencia_atividade**: Frequencia da atividade fisica:
+  - sedentario (nada ou pouca atividade fisica)
+  - atleta_esporadico (1-3 dias por semana)
+  - ativo (3-5 dias por semana)
+  - muito ativo (5-7 dias por semana)
+  - muito ativa e intensa (diária, incluindo exercícios pesados)
+- **percentual_gordura**: Percentual de gordura(Opcional)
+- **estado_fisico**: Se não informar percentual, escolha uma opção:
+  - muito_magro = 0
+  - magro = 1
+  - normal = 2
+  - sobrepeso = 3
+  - obeso = 4
+
+
 
 ### Resposta:
 Retorna os valores dos macronutrientes recomendados (proteínas, gorduras e carboidratos) em gramas e seus respectivos percentuais em relação ao total de calorias ajustadas, além do gasto basal e total de calorias.
